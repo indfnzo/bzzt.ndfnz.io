888aa40285409c37a7274597b90acc9b3eaa5de0
Jerome Indefenzo <jeromeindefenzo@gmail.com>
2020-08-19 07:08:49 +0800

    serve static client build via koajs

--- server/src/appconfig.ts
+++ server/src/appconfig.ts
@@ -3,7 +3,10 @@ const appconfig = {
 	sqlite_root: "C:\\Users\\jerome.indefenzo\\Documents\\Projects\\buzzer\\server\\runtime",
 
 	// Once an admin leaves, how long should we wait before deleting the room?
-	room_timeout: 60*60*5 // 5 minutes
+	room_timeout: 60*60*5, // 5 minutes
+
+	// Where are the static build files located?
+	client_build_path: '../client/build'
 }
 
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,26 @@
+import path from 'path';
 import Koa from 'koa';
 import cors from '@koa/cors';
+import serve from 'koa-static';
 import bodyParser from 'koa-bodyparser';
 import http from 'http';
 import socketIO from 'socket.io';
 
+import config from './appconfig';
 import setupSocketServer from './socket/setupSocketServer';
 import roomRoutes from './routes/rooms';
 
 const app = new Koa();
-const server = http.createServer(app.callback());
-
-const io = socketIO(server, { serveClient: false });
-setupSocketServer(io);
 
 app.use(cors());
 app.use(bodyParser());
 app.use(roomRoutes.routes());
+
+const clientPath = path.resolve(config.client_build_path);
+app.use(serve(clientPath, {}));
+
+const server = http.createServer(app.callback());
+const io = socketIO(server, { serveClient: false });
+setupSocketServer(io);
 
 server.listen(9000, '0.0.0.0');
